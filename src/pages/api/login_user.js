cef7b72c5dde6b8b6483dccf5048f83eaf1d591e
mehuu2000 <mehuu2000@gmail.com>
2024-08-28 16:02:50 +0900

    一旦push

--- src/pages/api/login_user.js
+++ src/pages/api/login_user.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
     try {
         // データベースからコードを検索
-        const user = await prisma.user.findFirst({
+        const user = await prisma.user.findUnique({
             where: { code: code }
         });
 
